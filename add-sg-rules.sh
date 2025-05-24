9d646f1d157cf5dbd54d34ca2996137730b907ab
Atul Kamble <atul_kamble@hotmail.com>
2025-05-24 11:29:13 +0530

    Create add-sg-rules.sh

---
+++ add-sg-rules.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SG_ID="sg-0d91531613c3e6618"
+
+# Allow HTTP (port 80)
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID \
+  --protocol tcp \
+  --port 80 \
+  --cidr 0.0.0.0/0
+
+# Allow HTTPS (port 443)
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID \
+  --protocol tcp \
+  --port 443 \
+  --cidr 0.0.0.0/0
+
+# Allow SSH (port 22)
+aws ec2 authorize-security-group-ingress \
+  --group-id $SG_ID \
+  --protocol tcp \
+  --port 22 \
+  --cidr 0.0.0.0/0
